2746dd603790550231bd2f123b8ca683c327aa5b
hahoang <havanit@gmail.com>
2016-07-06 09:30:21 +0700

    Update to Angular 2 RC4

--- src/Angular2FirstApp/wwwroot/systemjs.config.js
+++ src/Angular2FirstApp/wwwroot/systemjs.config.js
@@ -1,33 +1,32 @@
 ﻿(function (global) {
     // map tells the System loader where to look for things
-    var map = {
-        'app': 'js/app', // 'dist',
-        '@angular': 'lib/@angular',
-        'rxjs': 'lib/rxjs'
-    };
-    // packages tells the System loader how to load when no filename and/or no extension
-    var packages = {
-        'app': { main: 'main.js', defaultExtension: 'js' },
-        'rxjs': { defaultExtension: 'js' }
-    };
-    var ngPackageNames = [
-       'common',
-       'compiler',
-       'core',
-       'http',
-       'platform-browser',
-       'platform-browser-dynamic',
-       'router',
-       'router-deprecated',
-       'upgrade',
-    ];
-    // Add package entries for angular packages
-    ngPackageNames.forEach(function (pkgName) {
-        packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-    });
-    var config = {
-        map: map,
-        packages: packages
-    }
-    System.config(config);
+   var map = {
+      'app': 'js/app', // 'dist',
+      '@angular': 'lib/@angular',
+      'rxjs': 'lib/rxjs'
+   };
+   // packages tells the System loader how to load when no filename and/or no extension
+   var packages = {
+      'app': { main: 'main.js', defaultExtension: 'js' },
+      'rxjs': { defaultExtension: 'js' }
+   };
+   var packageNames = [
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/http',
+      '@angular/platform-browser',
+      '@angular/platform-browser-dynamic',
+      '@angular/router'
+   ];
+
+   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+   packageNames.forEach(function (pkgName) {
+      packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+   });
+   var config = {
+      map: map,
+      packages: packages
+   }
+   System.config(config);
 })(this);
